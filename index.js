0c5ccc6da7f40fd422fb0f3561617c7e9cb8c511
MarcelDev <marceldev96@gmail.com>
2023-12-06 12:12:52 +0100

    add Send

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Mfoulou Yves Marcel'))
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
